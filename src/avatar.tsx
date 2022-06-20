22bd17f1271fa6e2ec1bc03d4b25d0c946a62639
Brian Schlenker <bschlenk@umich.edu>
2022-06-20 15:16:47 -0400

    fix avatar badge

--- src/avatar.tsx
+++ src/avatar.tsx
@@ -28,27 +28,30 @@ export function Avatar({ user, badge, active, key }: Props) {
     return image;
   }
 
+  badge = '' + badge;
+
   return (
     <Frame key={key} width={SIZE} height={SIZE}>
       {image}
-      <Frame
+      <AutoLayout
         x={64}
         y={4}
-        width={20}
-        height={20}
         fill="#52b6ff"
         cornerRadius={1000}
+        width={20 + badge.length * 5}
+        verticalAlignItems="center"
+        horizontalAlignItems="end"
+        padding={{ horizontal: 4, top: 1 }}
       >
         <Text
           fill="#fff"
           height={20}
-          width={20}
+          width="fill-parent"
           horizontalAlignText="center"
-          x={4}
         >
           {badge}
         </Text>
-      </Frame>
+      </AutoLayout>
     </Frame>
   );
 }
